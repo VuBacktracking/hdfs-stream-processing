414716d7238fa1b467bf9c505d6df10b51ac9e61
vuphan <vhoang9999@gmail.com>
2024-07-29 23:31:54 +0700

    Update data generator

--- data_generator/dataframe_to_kafka.py
+++ data_generator/dataframe_to_kafka.py
@@ -33,17 +33,31 @@ class DataFrameToKafka:
     def __init__(self, input, sep, kafka_sep, row_sleep_time, source_file_extension, bootstrap_servers,
                  topic, repeat, shuffle, key_index, excluded_cols, limit_rows):
         self.input = input
+        logger.info(f"Input file: {self.input}")
         self.sep = sep
+        logger.info(f"CSV separator: {self.sep}")
         self.kafka_sep = kafka_sep
+        logger.info(f"Kafka separator: {self.kafka_sep}")
         self.row_sleep_time = row_sleep_time
+        logger.info(f"Row sleep time: {self.row_sleep_time}")
         self.repeat = repeat
+        logger.info(f"Repeat: {self.repeat}")
         self.shuffle = shuffle
+        logger.info(f"Shuffle: {self.shuffle}")
         self.excluded_cols = excluded_cols
+        logger.info(f"Excluded columns: {self.excluded_cols}")
         self.limit_rows = limit_rows
+        logger.info(f"Limit rows: {self.limit_rows}")
         self.df = self.read_source_file(source_file_extension)
         self.topic = topic
+        logger.info(f"Kafka topic: {self.topic}")
         self.key_index = key_index
-        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        logger.info(f"Key index: {self.key_index}")
+        logger.info(f"Bootstrap servers: {bootstrap_servers}")
+        try:
+            self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        except:
+            logger.info("No Broker available")
 
     def turn_df_to_str(self, df):
         """
@@ -66,6 +80,7 @@ class DataFrameToKafka:
 
         # Exclude columns and create the value column
         columns_to_write = [col for col in df.columns if col not in self.excluded_cols]
+        logger.info(f"Columns to write: {columns_to_write}")
         df = df.select(columns_to_write)
         df = df.withColumn('value', self.turn_df_to_str(df))
 
@@ -77,14 +92,13 @@ class DataFrameToKafka:
 
     def df_to_kafka(self):
         sayac = 0
-        repeat_counter = 0
         df_size = self.df.count() * self.repeat
         total_time = self.row_sleep_time * df_size
 
         for i in range(0, self.repeat):
-            for row in self.df.rdd.collect():
+            for row in self.df.collect():
                 key = str(row[self.key_index]).encode() if self.key_index != 1000 else str(row[0]).encode()
-                value = row[-1].encode()
+                value = row['value'].encode()
                 self.producer.send(self.topic, key=key, value=value)
                 self.producer.flush()
                 time.sleep(self.row_sleep_time)
@@ -92,6 +106,7 @@ class DataFrameToKafka:
                 remaining_per = 100 - (100 * (sayac / df_size))
                 remaining_time_secs = (total_time - (self.row_sleep_time * sayac))
                 remaining_time_mins = remaining_time_secs / 60
+                logger.info(f"{key.decode()} - {value.decode()}")
                 logger.info(f"{sayac}/{df_size} processed, {remaining_per:.2f}% remaining, "
                             f"{remaining_time_mins:.2f} minutes estimated time left.")
 
